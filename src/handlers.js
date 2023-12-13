da50de23e65042c271e8e865a64a69f96759c164
Vladislav <vladislav.work98@gmail.com>
2023-12-13 13:17:38 +0200

    Added load and handleClick methods to interact with the application

--- src/handlers.js
+++ src/handlers.js
@@ -1,11 +1,31 @@
 import Api from './api';
+import DomStuff from './domstuff';
 
 class Handlers {
   #api = new Api();
 
+  #domStuff = new DomStuff();
+
   #searchButton = document.querySelector('.additional__button');
 
-  handleClick = () => {};
+  #searchInput = document.querySelector('.additional__input input');
+
+  load = async (city = 'Paris') => {
+    this.#domStuff.loading('loading');
+    const weatherData = await this.#api.getWeatherData(city);
+    this.#domStuff.renderApp(weatherData);
+    this.#domStuff.loading('finished');
+  };
+
+  #handleClick = () => {
+    const input = this.#searchInput.value.trim();
+    if (input === '') return;
+    this.load(input);
+  };
+
+  clickHandler = () => {
+    this.#searchButton.addEventListener('click', this.#handleClick);
+  };
 }
 
 export default Handlers;
