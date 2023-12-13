b9e1a4d4c001fe4db9073d1f165b30c7851ca8cd
Vladislav <vladislav.work98@gmail.com>
2023-12-13 10:47:52 +0200

    Handlers class to handle the event

---
+++ src/handlers.js
@@ -0,0 +1,11 @@
+import Api from './api';
+
+class Handlers {
+  #api = new Api();
+
+  #searchButton = document.querySelector('.additional__button');
+
+  handleClick = () => {};
+}
+
+export default Handlers;
