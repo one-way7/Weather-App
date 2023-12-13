4d84fbad37bcc3ac291c161f1d72164e0cec25b4
Vladislav <vladislav.work98@gmail.com>
2023-12-13 11:34:47 +0200

    Added the loading method

--- src/domstuff.js
+++ src/domstuff.js
@@ -1,1 +1,17 @@
-class DomStuff {}
+class DomStuff {
+  #mainContent = document.querySelector('.current');
+
+  loading = (state) => {
+    const loaderSvg = document.querySelector('loading');
+    const errorBlock = document.querySelector('.error');
+
+    if (state === 'loading') {
+      loaderSvg.classList.remove('hide');
+      errorBlock.classList.add('hide');
+      this.#mainContent.classList.add('hide');
+    } else {
+      loaderSvg.classList.add('hide');
+      this.#mainContent.classList.add('remove');
+    }
+  };
+}
