038be1c95e83c6b7208031381acfce6926d53a50
Vladislav <vladislav.work98@gmail.com>
2023-12-13 13:18:35 +0200

    Added more methods to render weather data. Added setAnimation method, to set timeout delay

--- src/domstuff.js
+++ src/domstuff.js
@@ -15,10 +15,22 @@ class DomStuff {
     }
   };
 
+  #setAnimationTimeout = (elem) => {
+    setTimeout(() => {
+      elem.classList.remove('hide');
+    }, 100);
+
+    setTimeout(() => {
+      elem.classList.add('opac');
+    }, 255);
+  };
+
   #renderForecast = ({ city, current }) => {
     const currentWrapper = document.querySelector('.current__wrapper');
     const { time, tempC, weatherCondition, iconUrl } = current;
 
+    currentWrapper.classList.add('hide');
+    currentWrapper.classList.remove('opac');
     currentWrapper.textContent = '';
 
     currentWrapper.innerHTML = `
@@ -34,12 +46,73 @@ class DomStuff {
         <div class="current__weather-descr">${weatherCondition}</div>
       </div>
     `;
+
+    this.#setAnimationTimeout(currentWrapper);
+  };
+
+  #renderWeatherDetails = ({ current }) => {
+    const detailsDivContainer = document.querySelector('.additional__wrapper');
+    const { cloudy, windSpeed, humidity, rain } = current;
+
+    detailsDivContainer.classList.add('hide');
+    detailsDivContainer.classList.remove('opac');
+    detailsDivContainer.textContent = '';
+
+    detailsDivContainer.innerHTML = `
+      <div class="additional__item">
+        <div class="additional__detail-name">Cloudy</div>
+        <div class="additional__detail-value">${cloudy}%</div>
+      </div>
+
+      <div class="additional__item">
+        <div class="additional__detail-name">Humidity</div>
+        <div class="additional__detail-value">${humidity}%</div>
+      </div>
+
+      <div class="additional__item">
+        <div class="additional__detail-name">Wind</div>
+        <div class="additional__detail-value">${windSpeed}km/h</div>
+      </div>
+
+      <div class="additional__item">
+        <div class="additional__detail-name">Rain</div>
+        <div class="additional__detail-value">${rain}mm</div>
+      </div>
+    `;
+
+    this.#setAnimationTimeout(detailsDivContainer);
+  };
+
+  #renderDailyForecast = ({ daily }) => {
+    const dailyForecastWrapper = document.querySelector('.daily__wrapper');
+    let dailyForecastTemplate = '';
+
+    dailyForecastWrapper.classList.add('hide');
+    dailyForecastWrapper.classList.remove('opac');
+    dailyForecastWrapper.textContent = '';
+
+    daily.forEach((item) => {
+      const { day, minTempC, maxTempC, iconUrl } = item;
+
+      dailyForecastTemplate += `
+        <div class="daily__item">
+          <p class="daily_day">${day}</p>
+          <div class="daily__icon">
+            <img src="${iconUrl}" alt="weather icon">
+          </div>
+          <div class="daily_degrees">${minTempC}&deg; / ${maxTempC}&deg;</div>
+        </div>
+      `;
+    });
+
+    dailyForecastWrapper.innerHTML = dailyForecastTemplate;
+    this.#setAnimationTimeout(dailyForecastWrapper);
   };
 
   renderApp = (data) => {
     const error = document.querySelector('.error');
     const errorMessage = document.querySelector('.error__text');
-    console.log(data);
+
     if (data.error) {
       this.#mainContent.classList.add('hide');
       error.classList.remove('hide');
@@ -50,6 +123,8 @@ class DomStuff {
       errorMessage.textContent = '';
 
       this.#renderForecast(data);
+      this.#renderWeatherDetails(data);
+      this.#renderDailyForecast(data);
     }
   };
 }
